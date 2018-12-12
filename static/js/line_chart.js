4fdfea2464f5407dbf8325f639882b0bf37c9070
BeauWB <beauwbatchelor@gmail.com>
2018-12-11 17:46:47 -0800

    adding powerpoint and changes to html

--- static/js/line_chart.js
+++ static/js/line_chart.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 d3.json("/scatter_data",
     function(month_data){
         let layout = {
@@ -7,3 +9,54 @@ d3.json("/scatter_data",
     Plotly.plot("lineChart", month_data, layout);
     }
 );
+
+class Card extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      showMenu: false,
+    };
+    
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+  
+  showMenu(event) {
+    event.preventDefault();
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  
+  closeMenu() {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.showMenu}>
+          Show menu
+        </button>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div className="menu">
+                <button> Menu item 1 </button>
+                <button> Menu item 2 </button>
+                <button> Menu item 3 </button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
+  }
+}
